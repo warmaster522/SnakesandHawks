0141aaf69e1570c5b9a8a885b385d225704be727
davidcloak <35105111+davidcloak@users.noreply.github.com>
2021-09-17 22:13:56 -0400

    Unity Project Made
    Made the project and started planning the execution of dealing/shuffling.

---
+++ SnakesAndHawksUnity/Assets/Scripts/Deck.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    // Card 3 is Snake Card 8 is Hawk
+    public float Players = 2f;
+    public float Teams = 2f;
+    public float CardsPerColorInPlay = 8f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+// Gives the player a card
+    void GiveCard(){
+
+    }
+
+// Deals the deck at the begining
+    void DealDeck(){
+
+    }
+
+// Randimizes the cards placement 
+    void Shuffle(){
+
+    }
+}
